948e13bc45da525ba2b517fbd331cc31c73516d4
Yaroslav Zabara <yaroslav.zabara@waveaccess.ru>
2018-11-13 11:25:03 +0300

    I can use sys module.

---
+++ hello_world/sys_example.py
@@ -0,0 +1,17 @@
+import sys
+
+__version__ = '0.1'
+
+
+def print_console_arguments():
+    for arg in sys.argv:
+        print arg
+
+
+def print_path_variable():
+    print sys.path
+
+
+if __name__ == '__main__':
+    print_console_arguments()
+    print_path_variable()
